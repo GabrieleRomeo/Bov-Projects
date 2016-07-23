a2af0bcbb646071ce45f3395173ad0968cb7cbcc
Gabriele Romeo <gabriele.romeo@gmail.com>
2016-07-23 15:51:11 +0200

    Remove trailing spaces and fix indentations

--- js/project_4_search.js
+++ js/project_4_search.js
@@ -2,80 +2,74 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
-      var valid       = Validators;
-      var searchForm  = document.getElementById('search');
-      var doc         = document;
+    var valid  = validator,
+        $      = valid.$;
 
-      var inputs = {
-        searchTxt:  valid.getNode(doc.getElementById('searchTxt')),
-        category:   valid.getNode(doc.getElementById('category'))
-      }
+    var searchForm  = $('#search');
+
+    var inputs = {
+        searchTxt:  valid.getNode($('#searchTxt')),
+        category:   valid.getNode($('#category'))
+    }
 
       /*
-       * For each input, defines a particular Validator function that must 
-       * be satisfied to proceed further. 
+       * For each input, defines a particular Validator function that must
+       * be satisfied to proceed further.
        */
-       inputs.searchTxt.setValidator(function(elem) {
+        inputs.searchTxt.setValidator(function(elem) {
 
-          var constr = elem.constr;
-          var value  = elem.node.value;
+            var constr = elem.constr;
+            var value  = elem.node.value;
 
-          if (valid.isEmpty(value)) {
-              elem.node.setCustomValidity(constr.value);
-          } else {
-              elem.resetCustomValidity();
-          }
+            if (valid.isEmpty(value)) {
+                elem.node.setCustomValidity(constr.value);
+            } else {
+                elem.resetCustomValidity();
+            }
+        })
 
-       })
+        inputs.category.setConstraint('Please, select at least one Category');
+        inputs.category.setValidator(function(elem) {
 
-       inputs.category.setConstraint('Please, select at least one Category');
-       inputs.category.setValidator(function(elem) {
+            var node = elem.node;
 
-          var node = elem.node;
-          
-          if (valid.isEmpty(node.options[node.selectedIndex].value)) {
-              node.setCustomValidity(this.constrValue);
-          } else {
-              elem.resetCustomValidity();
-          }
+            if (valid.isEmpty(node.options[node.selectedIndex].value)) {
+                node.setCustomValidity(this.constrValue);
+            } else {
+                elem.resetCustomValidity();
+            }
+        })
 
-       })
-
-      /* Sets an initial custom validity for every input 
+      /* Sets an initial custom validity for every input
        * All the indicated inputs are not valid by default
        */
 
-      for (var input in inputs) {
-        if (inputs.hasOwnProperty(input)) {
-          if (inputs[input].constr.isRequired) {
-            inputs[input].node.setCustomValidity(
+        for (var input in inputs) {
+            if (inputs.hasOwnProperty(input)) {
+                if (inputs[input].constr.isRequired) {
+                    inputs[input].node.setCustomValidity(
                                               inputs[input].constr.constrValue);
-          }
-          
+                }
           // Using a closure, it defines a custom event listener for every
           // single inputs object
-          (function(input) {
-            input.node.addEventListener('change', function (event) {
-              
-                var constr  = input.constr;
-                input.setInvalidClass();
-                constr.check();
-            })
-          })(inputs[input]);
+            (function(input) {
+                input.node.addEventListener('change', function (event) {
+                    input.setInvalidClass();
+                    input.constr.check();
+                })
+            })(inputs[input]);
+            }
         }
-      }
 
-      searchForm.addEventListener('submit', function (event) {
-
-      // Check the inputs validity. If at least one input is invalid, stop
-      // the form
-      for (var input in inputs) {
-          if (inputs.hasOwnProperty(input)) {
-              if (!inputs[input].node.validity.valid) {
-                  event.preventDefault();
-              }
-          }
-      }
-
-      }, false);
+        searchForm.addEventListener('submit', function (event) {
+        // Check the inputs validity. If at least one input is invalid, stop
+        // the form
+            for (var input in inputs) {
+                if (inputs.hasOwnProperty(input)) {
+                    if (!inputs[input].node.validity.valid) {
+                        event.preventDefault();
+                    }
+                }
+            }
+        }, false);
 });
