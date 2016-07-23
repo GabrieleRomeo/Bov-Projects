3871f0c59f5c335f4ac5c8cc2d92dd72a97faf8b
Gabriele Romeo <gabriele.romeo@gmail.com>
2016-07-23 16:11:58 +0200

    Fix indentations and trailing spaces

--- js/project_4_login.js
+++ js/project_4_login.js
@@ -1,85 +1,82 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
-    var valid       = Validators;
-    var loginForm   = document.getElementById('loginForm');
-    var doc         = document;
+
+    var valid  = validator,
+        $      = valid.$;
+
+    var loginForm   = $('#loginForm');
 
     var inputs = {
-        username:  valid.getNode(doc.getElementById('username')),
-        password:  valid.getNode(doc.getElementById('password'))
+        username:  valid.getNode($('#username')),
+        password:  valid.getNode($('#password'))
     }
 
       /*
-       * For each input, defines a particular Validator function that must 
-       * be satisfied to proceed further. 
+       * For each input, defines a particular Validator function that must
+       * be satisfied to proceed further.
        */
 
-       inputs.username.setConstraint('Please, provide a valid username \n' +
+    inputs.username.setConstraint('Please, provide a valid username \n' +
         'The username must be a nonempty string and should be at least 3 ' +
         'characters in length.');
 
-       inputs.username.setValidator(function(elem) {
+    inputs.username.setValidator(function(elem) {
 
-          var constr = elem.constr;
-          var value  = elem.node.value;
+        var value  = elem.node.value;
 
-          if (valid.isLength(value, 2)) {
-              elem.node.setCustomValidity(constr.value);
-          } else {
-              elem.resetCustomValidity();
-          }
+        if (valid.isLength(value, 2)) {
+            elem.node.setCustomValidity(elem.constr.value);
+        } else {
+            elem.resetCustomValidity();
+        }
+    })
 
-       })
+    inputs.password.setValidator(function(elem) {
 
-       inputs.password.setValidator(function(elem) {
+        var value  = elem.node.value;
 
-          var constr = elem.constr;
-          var value  = elem.node.value;
+        if (valid.isLength(value, 5)) {
+            elem.node.setCustomValidity(elem.constr.value);
+        } else {
+            elem.resetCustomValidity();
+        }
 
-          if (valid.isLength(value, 5)) {
-              elem.node.setCustomValidity(constr.value);
-          } else {
-              elem.resetCustomValidity();
-          }
+    })
 
-       })
-
-      /* Sets an initial custom validity for every input 
+      /* Sets an initial custom validity for every input
        * All the indicated inputs are not valid by default
        */
 
-      for (var input in inputs) {
+    for (var input in inputs) {
         if (inputs.hasOwnProperty(input)) {
-          if (inputs[input].constr.isRequired) {
-            inputs[input].node.setCustomValidity(
-                                              inputs[input].constr.constrValue);
-          }
-          
-          // Using a closure, it defines a custom event listener for every
-          // single inputs object
-          (function(input) {
-            input.node.addEventListener('change', function (event) {
-              
-                var constr  = input.constr;
-                input.setInvalidClass();
-                constr.check();
-            })
-          })(inputs[input]);
+            if (inputs[input].constr.isRequired) {
+                inputs[input].node.setCustomValidity(
+                                                  inputs[input].constr.constrValue);
+            }
+
+              // Using a closure, it defines a custom event listener for every
+              // single inputs object
+            (function(input) {
+                input.node.addEventListener('change', function (event) {
+
+                    var constr  = input.constr;
+                    input.setInvalidClass();
+                    constr.check();
+                })
+            })(inputs[input]);
         }
-      }
+    }
 
-      loginForm.addEventListener('submit', function (event) {
-
-      // Check the inputs validity. If at least one input is invalid, stop
-      // the form
-      for (var input in inputs) {
-          if (inputs.hasOwnProperty(input)) {
-              if (!inputs[input].node.validity.valid) {
-                  event.preventDefault();
-              }
-          }
-      }
-
-      }, false);
+    loginForm.addEventListener('submit', function (event) {
+        // Check the inputs validity. If at least one input is invalid, stop
+        // the form
+        for (var input in inputs) {
+            if (inputs.hasOwnProperty(input)) {
+                if (!inputs[input].node.validity.valid) {
+                    event.preventDefault();
+                }
+            }
+        }
+    }, false);
 });
