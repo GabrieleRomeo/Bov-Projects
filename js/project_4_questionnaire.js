c6744972dbe8c6e2fc6e1b8fec8a61a535a258ea
Gabriele Romeo <gabriele.romeo@gmail.com>
2016-07-23 16:05:43 +0200

    Fix issue with options selector

--- js/project_4_questionnaire.js
+++ js/project_4_questionnaire.js
@@ -2,28 +2,30 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
-      var valid       = Validators;
-      var signupForm  = document.getElementById('questionnaire');
-      var doc         = document;
-      var options     = doc.getElementsByName('option');
+    var valid  = validator,
+        $      = valid.$,
+        $$     = valid.$$;
 
-      var inputs = {
-        otherValue: valid.getNode(doc.getElementById('otherValue'))
-      }
+    var signupForm  = $('#questionnaire');
+    var options     = $$('[name="option"]');
+
+    var inputs = {
+        otherValue: valid.getNode($('#otherValue'))
+    };
 
       // Sets the change event for all the radio buttons
-      var newObject = Object.keys(options).map(function(elem) {
-          options[elem].addEventListener('change', function (event) {
+    Object.keys(options).map(function(elem) {
+        options[elem].addEventListener('change', function () {
 
             var input     = inputs.otherValue;
             var classHidd = 'list__input--hidden';
             var re        = new RegExp(classHidd);
             var className = input.node.className.replace(re,'');
-            
-            /* 
-             * When the radio button with ID 'other' is selected we need 
-             * to set the constraint for the otherValue textbox
-             */
+
+        /*
+         * When the radio button with ID 'other' is selected we need
+         * to set the constraint for the otherValue textbox
+         */
             if (this.getAttribute('id') === 'other' && this.checked) {
                 input.node.setCustomValidity(input.constr.value);
                 input.node.className = className;
@@ -34,51 +36,44 @@ document.addEventListener('DOMContentLoaded', function () {
                     input.resetCustomValidity();
                 }
             }
+        });
+    });
 
-          });
-      });
+    inputs.otherValue.setValidator(function(elem) {
 
-      inputs.otherValue.setValidator(function(elem) {
+        var node   = elem.node,
+            constr = elem.constr,
+            value  = node.value;
 
-          var node   = elem.node,
-              constr = elem.constr,
-              value  = node.value;
-
-          if (valid.isEmpty(value)) {
-                node.setCustomValidity(constr.value);
-          } else if (!valid.isOfLength(value, 2)) {
-            node.setCustomValidity('The length of this field should ' + 
+        if (valid.isEmpty(value)) {
+            node.setCustomValidity(constr.value);
+        } else if (!valid.isOfLength(value, 2)) {
+            node.setCustomValidity('The length of this field should ' +
                                    'not be lower than two characters');
-          }
-            else {
-                elem.resetCustomValidity();
-          }
-
-      })
-
-
-      inputs.otherValue.node.addEventListener('change', function (event) {
-
-            var node   = inputs[this.getAttribute('id')],
-                constr = node.constr;
-
-            node.setInvalidClass();
-            constr.check();
-
-      });
+        }
+        else {
+            elem.resetCustomValidity();
+        }
+    });
 
 
-      signupForm.addEventListener('submit', function (event) {
+    inputs.otherValue.node.addEventListener('change', function () {
+        var node   = inputs[this.getAttribute('id')];
 
-      // Check the validity. If at least one input is invalid, stop
-      // the submit event
-      for (var input in inputs) {
-          if (inputs.hasOwnProperty(input)) {
-              if (!inputs[input].node.validity.valid) {
-                  event.preventDefault();
-              }
-          }
-      }
+        node.setInvalidClass();
+        node.constr.check();
+    });
 
-      }, false);
+
+    signupForm.addEventListener('submit', function (event) {
+        // Check the validity. If at least one input is invalid, stop
+        // the submit event
+        for (var input in inputs) {
+            if (inputs.hasOwnProperty(input)) {
+                if (!inputs[input].node.validity.valid) {
+                    event.preventDefault();
+                }
+            }
+        }
+    }, false);
 });
